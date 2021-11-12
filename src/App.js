4eb4a05cdf349f80a2c3eebf04417b78dfa2a76f
phamtruonghoaithanh0210 <1851050129thanh@ou.edu.vn>
2021-11-13 01:52:24 +0700

    intall MUI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {ThemeProvider, createTheme} from "@mui/material"
+import {BrowserRouter, Route, Link} from "react-router-dom"
+import Header from './header/Header';
+import Body from './body/Body'
 
+const theme = createTheme();
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Header/>
+        <Body />
+      </BrowserRouter>
+    </ThemeProvider>
+    
+  )
 }
 
 export default App;
