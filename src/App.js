9c159a06e449a1007603bd65fad8f3a81e412c17
phamtruonghoaithanh0210 <1851050129thanh@ou.edu.vn>
2021-11-23 21:38:09 +0700

    Add react-spinners and make loading.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React,{useState, useEffect} from 'react';
 import './App.css';
 import {ThemeProvider, createTheme} from "@mui/material"
 import {BrowserRouter,Routes, Route} from "react-router-dom"
@@ -8,25 +9,50 @@ import EducationH from './header/EducationH';
 import ExperienceH from './header/ExperienceH';
 import ProfileH from './header/ProfileH';
 import ActivityH from './header/ActivityH';
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
 
 const theme = createTheme();
 function App() {
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    setLoading(true)
+    setTimeout( () => {
+      setLoading(false)
+    },8000)
+  }, [])
   return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Header/>
-        <Routes>
-          <Route exact path="/" element={<Body/>} />
-          <Route exact  path="/home" element={<Body/>} />
-          <Route exact  path="/education" element={<EducationH/>} />
-          <Route exact  path="/experience" element={<ExperienceH/>} />
-          <Route exact  path="/profile" element={<ProfileH/>} />
-          <Route exact  path="/activity" element={<ActivityH/>} />
-        </Routes>
-        <Footer/>
-      </BrowserRouter>
-    </ThemeProvider>
-    
+    <>
+    {loading ? (
+      <div className="App">
+      <ClimbingBoxLoader 
+        size={30}
+        color={"#ff8ba7"}
+        loading={loading}
+      />
+
+      <ClimbingBoxLoader 
+        size={30}
+        color={"#ff8ba7"}
+        loading={loading}
+      />
+      </div>
+    ) : (
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Header/>
+          <Routes>
+            <Route exact path="/" element={<Body/>} />
+            <Route exact  path="/home" element={<Body/>} />
+            <Route exact  path="/education" element={<EducationH/>} />
+            <Route exact  path="/experience" element={<ExperienceH/>} />
+            <Route exact  path="/profile" element={<ProfileH/>} />
+            <Route exact  path="/activity" element={<ActivityH/>} />
+          </Routes>
+          <Footer/>
+        </BrowserRouter>
+      </ThemeProvider>
+    )}
+    </>
   )
 }
 
